ab33dbd0d6addeb792d4730ee578e1c69c7e4baa
ashik-mia <ashikdeone908@gmail.com>
2025-02-06 06:16:40 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./*.html"], // Adjust if needed
+    theme: {
+      extend: {
+        colors:{
+            'tiya-green':'#ABEF5F',
+            'kalo':'#212121',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
